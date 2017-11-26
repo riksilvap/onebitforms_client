2c215f98743e55b0dd6812d81803680dea2d0e6c
Ricardo Silva <riksilvap@gmail.com>
2017-11-26 02:15:59 -0200

    Setup materialize charts colorpick

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
-FROM node:9
+FROM node:9.2-alpine
 ENV INSTALL_PATH /onebitforms-client
 RUN npm install -g @angular/cli
 RUN mkdir -p $INSTALL_PATH
 WORKDIR $INSTALL_PATH
-COPY package.json .
 COPY . .
 RUN npm install
 CMD ["npm", "start"]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MaterializeModule } from 'ng2-materialize';
+import { ColorPickerModule } from 'ngx-color-picker';
+import { ChartsModule } from 'ng2-charts';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule.forRoot(),
+    ColorPickerModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
